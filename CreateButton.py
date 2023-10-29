725963ac80ab9b35b4b7c9f0650275158db83d0a
Nandxr <nandorrexexel@gmail.com>
2023-10-29 19:13:24 +0100

    Update text in the text box

--- CreateButton.py
+++ CreateButton.py
@@ -1,7 +1,7 @@
 from tkinter import Button
 
-def create_button(window, text, x, y, height, width, font=("Helvetica", 20), bg=None):
-    button = Button(window, text=text, font=font)
+def create_button(window, text, x, y, height, width, font=("Helvetica", 20), bg=None, command=None):
+    button = Button(window, text=text, font=font, command=command)
     button.place(x=x, y=y, height=height, width=width)
     if bg:
         button.config(bg=bg)
--- Main.py
+++ Main.py
@@ -1,27 +1,48 @@
 from tkinter import *
 from CreateButton import create_button
 
+text = ""
+def update_text(x):
+    global text
+    text += str(x)
+    text_box.delete(1.0, "end")
+    text_box.insert(1.0, text)
+def eval_numbers():
+    global text
+    try:
+        text = str(eval(text))
+        text_box.delete(1.0, "end")
+        text_box.insert(1.0, text)
+    except:
+        clear_text_box()
+        text_box.insert(1.0, "Error")
+
+def clear_text_box():
+    global text
+    text = ""
+    text_box.delete(1.0, "end")
+
 window = Tk()
 window.title("Számológép")
 window.geometry("250x380")
 text_box = Text(window, font=("Helvetica", 24))
 text_box.place(x=10, y=10, height=40, width=230)
 
-Seven = create_button(window, "7", 10, 60, 50, 50)
-Eight = create_button(window, "8", 70, 60, 50, 50)
-Nine = create_button(window, "9", 130, 60, 50, 50)
-Four = create_button(window, "4", 10, 120, 50, 50)
-Five = create_button(window, "5", 70, 120, 50, 50)
-Six = create_button(window, "6", 130, 120, 50, 50)
-One = create_button(window, "1", 10, 180, 50, 50)
-Two = create_button(window, "2", 70, 180, 50, 50)
-Three = create_button(window, "3", 130, 180, 50, 50)
-Zero = create_button(window, "0", 10, 240, 50, 170)
-Add = create_button(window, "+", 190, 60, 50, 50)
-Minus = create_button(window, "-", 190, 120, 50, 50)
-Division = create_button(window, "/", 190, 180, 50, 50)
-Multiplication = create_button(window, "*", 190, 240, 50, 50)
-Equal = create_button(window, "=", 190, 300, 50, 50, bg="deepskyblue")
+Seven = create_button(window, "7", 10, 60, 50, 50, command=lambda: update_text(7))
+Eight = create_button(window, "8", 70, 60, 50, 50, command=lambda: update_text(8))
+Nine = create_button(window, "9", 130, 60, 50, 50, command=lambda: update_text(9))
+Four = create_button(window, "4", 10, 120, 50, 50, command=lambda: update_text(4))
+Five = create_button(window, "5", 70, 120, 50, 50, command=lambda: update_text(5))
+Six = create_button(window, "6", 130, 120, 50, 50, command=lambda: update_text(6))
+One = create_button(window, "1", 10, 180, 50, 50, command=lambda: update_text(1))
+Two = create_button(window, "2", 70, 180, 50, 50, command=lambda: update_text(2))
+Three = create_button(window, "3", 130, 180, 50, 50, command=lambda: update_text(3))
+Zero = create_button(window, "0", 10, 240, 50, 170, command=lambda: update_text(0))
+Add = create_button(window, "+", 190, 60, 50, 50, command=lambda: update_text("+"))
+Minus = create_button(window, "-", 190, 120, 50, 50, command=lambda: update_text("-"))
+Division = create_button(window, "/", 190, 180, 50, 50, command=lambda: update_text("/"))
+Multiplication = create_button(window, "*", 190, 240, 50, 50, command=lambda: update_text("*"))
+Equal = create_button(window, "=", 190, 300, 50, 50,bg="deepskyblue")
 C = create_button(window, "C", 130, 300, 50, 50)
 Save = create_button(window, "Eredmény mentése", 10, 300, 50, 110, font=("Helvetica", 8))
 
