1cf3520d567923cb750a0b900c454b69be0e6571
Nandxr <nandorrexexel@gmail.com>
2023-10-31 14:06:03 +0100

    Adding class to the codes

--- CreateButton.py
+++ CreateButton.py
@@ -1,8 +1,10 @@
 from tkinter import Button
 
-def create_button(window, text, x, y, height, width, font=("Helvetica", 20), bg=None, command=None):
-    button = Button(window, text=text, font=font, command=command)
-    button.place(x=x, y=y, height=height, width=width)
-    if bg:
-        button.config(bg=bg)
-    return button
+class Create_Button:
+    @classmethod
+    def create_buttons(cls, window, text, x, y, height, width, font=("Helvetica", 20), bg=None, command=None,):
+        button = Button(window, text=text, font=font, command=command)
+        button.place(x=x, y=y, height=height, width=width)
+        if bg:
+            button.config(bg=bg)
+        return button
--- Main.py
+++ Main.py
@@ -1,89 +1,93 @@
 from tkinter import *
-from CreateButton import create_button
+from CreateButton import Create_Button
 import math
 
-text = ""
+class CalculatorApp:
+    def __init__(self, window):
+        self.window = window
+        self.window.title("Számológép")
+        self.window.geometry("310x380")
+        self.window.resizable(False, False)
 
-#text változóban tárolt szöveg újraíródik a text_box-ban, amikor meghívjuk
-def update_text(x):
-    global text
-    text += str(x)
-    text_box.delete(1.0, "end") #törli a text_box-ban lévő szöveget
-    text_box.insert(1.0, text) #beilleszti a frissített text tartalmat a szövegdoboz elejére
+        self.text = ""
+        self.text_box = Text(self.window, font=("Helvetica", 24))
+        self.text_box.place(x=10, y=10, height=40, width=290)
 
-def eval_numbers():
-    global text
-    try:
-        text = str(eval(text)) #text változó értékének kiértékelése az eval() függvény segítségével.
-        #text_box.delete(1.0, "end")
-        text_box.insert(2.0, "=" + text)
-        return text
-    except:
-        clear_text_box()
-        text_box.insert(1.0, "Error")
-        return None
+        Create_Button.create_buttons(self.window, "7", 10, 60, 50, 50, command=lambda: self.update_text(7))
+        Create_Button.create_buttons(self.window, "8", 70, 60, 50, 50, command=lambda: self.update_text(8))
+        Create_Button.create_buttons(self.window, "9", 130, 60, 50, 50, command=lambda: self.update_text(9))
+        Create_Button.create_buttons(self.window, "4", 10, 120, 50, 50, command=lambda: self.update_text(4))
+        Create_Button.create_buttons(self.window, "5", 70, 120, 50, 50, command=lambda: self.update_text(5))
+        Create_Button.create_buttons(self.window, "6", 130, 120, 50, 50, command=lambda: self.update_text(6))
+        Create_Button.create_buttons(self.window, "1", 10, 180, 50, 50, command=lambda: self.update_text(1))
+        Create_Button.create_buttons(self.window, "2", 70, 180, 50, 50, command=lambda: self.update_text(2))
+        Create_Button.create_buttons(self.window, "3", 130, 180, 50, 50, command=lambda: self.update_text(3))
+        Create_Button.create_buttons(self.window, "0", 70, 240, 50, 50, command=lambda: self.update_text(0))
+        Create_Button.create_buttons(self.window, "(", 10, 240, 50, 50, command=lambda: self.update_text("("))
+        Create_Button.create_buttons(self.window, ")", 130, 240, 50, 50, command=lambda: self.update_text(")"))
+        Create_Button.create_buttons(self.window, "+", 190, 60, 50, 50, command=lambda: self.update_text("+"))
+        Create_Button.create_buttons(self.window, "-", 190, 120, 50, 50, command=lambda: self.update_text("-"))
+        Create_Button.create_buttons(self.window, "/", 190, 180, 50, 50, command=lambda: self.update_text("/"))
+        Create_Button.create_buttons(self.window, "*", 190, 240, 50, 50, command=lambda: self.update_text("*"))
+        Create_Button.create_buttons(self.window, "√", 250, 60, 110, 50, command=self.square_root)
+        Create_Button.create_buttons(self.window, "π", 250, 180, 50, 50, command=lambda: self.update_text(round(math.pi, 2)))
+        Create_Button.create_buttons(self.window, "²", 250, 240, 50, 50, command=self.square)
+        Create_Button.create_buttons(self.window, "=", 190, 300, 50, 110, bg="deepskyblue", command=self.eval_numbers)
+        Create_Button.create_buttons(self.window, "C", 130, 300, 50, 50, command=self.clear_text_box)
+        Create_Button.create_buttons(self.window, "Eredmény mentése", 10, 300, 50, 110, font=("Helvetica", 8), command=self.save_result)
 
-def clear_text_box():
-    global text
-    text = ""
-    text_box.delete(1.0, "end")
 
-def save_result():
-    calculation = text_box.get(1.0, "end-1c")  #A text_box tartalmának megszerzése
-    if calculation is not None:
-        with open("result.txt", "a") as file:
-            file.write("\nResult of the calculation:\n")
-            file.write(calculation)
-            text_box.delete(1.0, "end")
-            text_box.insert(1.0, "Result saved!")
 
-def square_root():
-    global text
-    try:
-        root = math.sqrt(float(text))
-        text_box.delete(1.0, "end")
-        text_box.insert(1.0, root)
-        return str(root)
-    except:
-        clear_text_box()
-        text_box.insert(1.0, "Error")
-        return None
+    #text változóban tárolt szöveg újraíródik a text_box-ban, amikor meghívjuk
+    def update_text(self, x):
+        self.text += str(x)
+        self.text_box.delete(1.0, "end") #törli a text_box-ban lévő szöveget
+        self.text_box.insert(1.0, self.text) #beilleszti a frissített text tartalmat a szövegdoboz elejére
 
-def square():
-    global text
-    squared = math.pow(float(text), 2)
-    text_box.delete(1.0, "end")
-    text_box.insert(1.0, squared)
-    return str(squared)
+    def eval_numbers(self):
+        try:
+            self.text = str(eval(self.text)) #text változó értékének kiértékelése az eval() függvény segítségével.
+            #text_box.delete(1.0, "end")
+            self.text_box.insert(2.0, "=" + self.text)
+            return self.text
+        except:
+            self.clear_text_box()
+            self.text_box.insert(1.0, "Error")
+            return None
 
-window = Tk() #Új thinkter ablak létrehozása és window változóhoz rendelése
-window.title("Számológép")
-window.geometry("310x380")
-window.resizable(False, False)
-text_box = Text(window, font=("Helvetica", 24))
-text_box.place(x=10, y=10, height=40, width=290)
+    def clear_text_box(self):
+        self.text = ""
+        self.text_box.delete(1.0, "end")
 
-Seven = create_button(window, "7", 10, 60, 50, 50, command=lambda: update_text(7))
-Eight = create_button(window, "8", 70, 60, 50, 50, command=lambda: update_text(8))
-Nine = create_button(window, "9", 130, 60, 50, 50, command=lambda: update_text(9))
-Four = create_button(window, "4", 10, 120, 50, 50, command=lambda: update_text(4))
-Five = create_button(window, "5", 70, 120, 50, 50, command=lambda: update_text(5))
-Six = create_button(window, "6", 130, 120, 50, 50, command=lambda: update_text(6))
-One = create_button(window, "1", 10, 180, 50, 50, command=lambda: update_text(1))
-Two = create_button(window, "2", 70, 180, 50, 50, command=lambda: update_text(2))
-Three = create_button(window, "3", 130, 180, 50, 50, command=lambda: update_text(3))
-Zero = create_button(window, "0", 70, 240, 50, 50, command=lambda: update_text(0))
-OpenBracket = create_button(window, "(", 10, 240, 50, 50, command=lambda: update_text("("))
-CloseBracket = create_button(window, ")", 130, 240, 50, 50, command=lambda: update_text(")"))
-Add = create_button(window, "+", 190, 60, 50, 50, command=lambda: update_text("+"))
-Minus = create_button(window, "-", 190, 120, 50, 50, command=lambda: update_text("-"))
-Division = create_button(window, "/", 190, 180, 50, 50, command=lambda: update_text("/"))
-Multiplication = create_button(window, "*", 190, 240, 50, 50, command=lambda: update_text("*"))
-SquareRoot = create_button(window, "√", 250, 60, 110, 50, command=square_root)
-Square = create_button(window, "π", 250, 180, 50, 50, command=lambda: update_text(round(math.pi, 2)))
-Pi = create_button(window, "²", 250, 240, 50, 50, command=square)
-Equal = create_button(window, "=", 190, 300, 50, 110,bg="deepskyblue",command=eval_numbers)
-C = create_button(window, "C", 130, 300, 50, 50,command=clear_text_box)
-Save = create_button(window, "Eredmény mentése", 10, 300, 50, 110, font=("Helvetica", 8), command=save_result)
+    def save_result(self):
+        calculation = self.text_box.get(1.0, "end-1c")  #A text_box tartalmának megszerzése
+        if calculation is not None:
+            with open("result.txt", "a") as file:
+                file.write("\nResult of the calculation:\n")
+                file.write(calculation)
+                self.text_box.delete(1.0, "end")
+                self.text_box.insert(1.0, "Result saved!")
 
-window.mainloop()
+    def square_root(self):
+        global text
+        try:
+            root = math.sqrt(float(self.text))
+            self.text_box.delete(1.0, "end")
+            self.text_box.insert(1.0, root)
+            return str(root)
+        except:
+            self.clear_text_box()
+            self.text_box.insert(1.0, "Error")
+            return None
+
+    def square(self):
+        global text
+        squared = math.pow(float(self.text), 2)
+        self.text_box.delete(1.0, "end")
+        self.text_box.insert(1.0, squared)
+        return str(squared)
+
+if __name__ == "__main__":
+    window = Tk()
+    app = CalculatorApp(window)
+    window.mainloop()
