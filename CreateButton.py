3cca1e6ba5e31d8cbd922cfeb811772a3086fd85
Virág Nándor <nandorrexexel@gmail.com>
2023-11-22 20:39:28 +0100

    Fixes

--- CreateButton.py
+++ CreateButton.py
@@ -1,8 +1,8 @@
 from tkinter import Button
 
 class Create_Button:
-    @classmethod
-    def create_buttons(cls, window, text, x, y, height, width, font=("Helvetica", 20), bg=None, command=None,):
+    @classmethod #nem szükséges az osztály példányosítása a használathoz
+    def create_buttons(cls, window, text, x, y, height=50, width=50, font=("Helvetica", 20), bg=None, command=None,):
         button = Button(window, text=text, font=font, command=command)
         button.place(x=x, y=y, height=height, width=width)
         if bg:
--- Main.py
+++ Main.py
@@ -4,6 +4,8 @@ from datetime import datetime
 import math
 
 class CalculatorApp:
+
+    #ablak és a szükséges grafikus elemek inicializálása
     def __init__(self, window):
         self.window = window
         self.window.title("Számológép")
@@ -14,27 +16,27 @@ class CalculatorApp:
         self.text_box = Text(self.window, font=("Helvetica", 24))
         self.text_box.place(x=10, y=10, height=40, width=290)
 
-        Create_Button.create_buttons(self.window, "7", 10, 60, 50, 50, command=lambda: self.update_text(7))
-        Create_Button.create_buttons(self.window, "8", 70, 60, 50, 50, command=lambda: self.update_text(8))
-        Create_Button.create_buttons(self.window, "9", 130, 60, 50, 50, command=lambda: self.update_text(9))
-        Create_Button.create_buttons(self.window, "4", 10, 120, 50, 50, command=lambda: self.update_text(4))
-        Create_Button.create_buttons(self.window, "5", 70, 120, 50, 50, command=lambda: self.update_text(5))
-        Create_Button.create_buttons(self.window, "6", 130, 120, 50, 50, command=lambda: self.update_text(6))
-        Create_Button.create_buttons(self.window, "1", 10, 180, 50, 50, command=lambda: self.update_text(1))
-        Create_Button.create_buttons(self.window, "2", 70, 180, 50, 50, command=lambda: self.update_text(2))
-        Create_Button.create_buttons(self.window, "3", 130, 180, 50, 50, command=lambda: self.update_text(3))
-        Create_Button.create_buttons(self.window, "0", 70, 240, 50, 50, command=lambda: self.update_text(0))
-        Create_Button.create_buttons(self.window, "(", 10, 240, 50, 50, command=lambda: self.update_text("("))
-        Create_Button.create_buttons(self.window, ")", 130, 240, 50, 50, command=lambda: self.update_text(")"))
-        Create_Button.create_buttons(self.window, "+", 190, 60, 50, 50, command=lambda: self.update_text("+"))
-        Create_Button.create_buttons(self.window, "-", 190, 120, 50, 50, command=lambda: self.update_text("-"))
-        Create_Button.create_buttons(self.window, "/", 190, 180, 50, 50, command=lambda: self.update_text("/"))
-        Create_Button.create_buttons(self.window, "*", 190, 240, 50, 50, command=lambda: self.update_text("*"))
+        Create_Button.create_buttons(self.window, "7", 10, 60, command=lambda: self.update_text(7))
+        Create_Button.create_buttons(self.window, "8", 70, 60, command=lambda: self.update_text(8))
+        Create_Button.create_buttons(self.window, "9", 130, 60,  command=lambda: self.update_text(9))
+        Create_Button.create_buttons(self.window, "4", 10, 120,  command=lambda: self.update_text(4))
+        Create_Button.create_buttons(self.window, "5", 70, 120,  command=lambda: self.update_text(5))
+        Create_Button.create_buttons(self.window, "6", 130, 120, command=lambda: self.update_text(6))
+        Create_Button.create_buttons(self.window, "1", 10, 180,  command=lambda: self.update_text(1))
+        Create_Button.create_buttons(self.window, "2", 70, 180,  command=lambda: self.update_text(2))
+        Create_Button.create_buttons(self.window, "3", 130, 180, command=lambda: self.update_text(3))
+        Create_Button.create_buttons(self.window, "0", 70, 240,  command=lambda: self.update_text(0))
+        Create_Button.create_buttons(self.window, "(", 10, 240,  command=lambda: self.update_text("("))
+        Create_Button.create_buttons(self.window, ")", 130, 240, command=lambda: self.update_text(")"))
+        Create_Button.create_buttons(self.window, "+", 190, 60,  command=lambda: self.update_text("+"))
+        Create_Button.create_buttons(self.window, "-", 190, 120, command=lambda: self.update_text("-"))
+        Create_Button.create_buttons(self.window, "/", 190, 180, command=lambda: self.update_text("/"))
+        Create_Button.create_buttons(self.window, "*", 190, 240, command=lambda: self.update_text("*"))
         Create_Button.create_buttons(self.window, "√", 250, 60, 110, 50, command=self.square_root)
-        Create_Button.create_buttons(self.window, "π", 250, 180, 50, 50, command=lambda: self.update_text(round(math.pi, 2)))
-        Create_Button.create_buttons(self.window, "²", 250, 240, 50, 50, command=self.square)
+        Create_Button.create_buttons(self.window, "π", 250, 180, command=lambda: self.update_text(round(math.pi, 2)))
+        Create_Button.create_buttons(self.window, "²", 250, 240, command=self.square)
         Create_Button.create_buttons(self.window, "=", 190, 300, 50, 110, bg="deepskyblue", command=self.eval_numbers)
-        Create_Button.create_buttons(self.window, "C", 130, 300, 50, 50, command=self.clear_text_box)
+        Create_Button.create_buttons(self.window, "C", 130, 300, command=self.clear_text_box)
         Create_Button.create_buttons(self.window, "Eredmény mentése", 10, 300, 50, 110, font=("Helvetica", 8), command=self.save_result)
 
 
@@ -63,12 +65,12 @@ class CalculatorApp:
     def save_result(self):
         calculation = self.text_box.get(1.0, "end-1c")  #A text_box tartalmának megszerzése
         if calculation is not None:
-            title = f"\nResult of the calculation ({datetime.now().strftime('%Y-%m-%d %H:%M:%S')}):\n"
+            title = f"\nA számítás eredménye ({datetime.now().strftime('%Y-%m-%d %H:%M:%S')}):\n"
             with open("result.txt", "a") as file:
                 file.write(title)
                 file.write(calculation)
                 self.text_box.delete(1.0, "end")
-                self.text_box.insert(1.0, "Result saved!")
+                self.text_box.insert(1.0, "Eredmény mentve!")
 
     def square_root(self):
         global text
@@ -92,6 +94,6 @@ class CalculatorApp:
         return self.text
 
 if __name__ == "__main__":
-    window = Tk()
-    app = CalculatorApp(window)
-    window.mainloop()
+    window = Tk() #ablak létrehozása
+    app = CalculatorApp(window) #Osztály példányosítása
+    window.mainloop() #Ablak főciklusának elindítása
